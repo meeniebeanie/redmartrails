b9495c67b4bab83f345121546b59ceacd8cdadab
meeniebeanie <minyen.j@gmail.com>
2016-07-30 17:04:30 +0800

    added home, help, about pages and done linking

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/about'
+  #Static Pages
+  root 'static_pages#home'
 
-  get 'static_pages/help'
+  get '/about', to: 'static_pages#about'
+
+  get '/help', to: 'static_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'products/new'
   get 'products/show'
 
-  get 'users/new'
-  get 'users/show'
+  get '/signup', to: 'users#new'
 
 
   #RESTFUL routes
